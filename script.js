8a44d250af1b34b17128d707016fcf508cd5e156
Rayan <rayanrocha@gmail.com>
2020-09-15 09:54:12 -0300

    Grafico em Barras

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    var clicks = 0;
+
+  $('input[id="MainContent_txtDataAbertura"]').after('<input type="button" name="botao-ok" value="Copiar Data" id="CopiarData" style="margin-top: 0px; margin-left: 5px; margin-right: 5px; " >');
+
+
+   
+  
+  });
